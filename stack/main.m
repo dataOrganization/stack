831a5b1adc760be6a31f03ffb1bc449a5181f1d8
mac <604240831@qq.com>
2016-05-06 15:14:44 +0800

    first

--- stack/main.m
+++ stack/main.m
@@ -8,10 +8,47 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+typedef int elemType;
+#define MAXSIZE 1024
+typedef struct stack{
+    elemType data[MAXSIZE];
+    int top;
+}Stack;
+
+Stack * initWithStack(Stack *s){
+    s = (Stack *)malloc(sizeof(Stack));
+    s->top = 0;
+    return s;
+}
+
+void insertWithStack(Stack *s ,elemType e){
+    s->top++;
+    s->data[s->top] = e;
+}
+
+void printfStack(Stack *s){
+    printf("%d \n",s->top);
+    for (int i = 1; i < s->top + 1; i++) {
+        printf("%d  ",s->data[i]);
     }
+    printf("\n");
+}
+
+void deleteStack(Stack *s ,elemType e) {
+    while (s->top) {
+        e = s->data[s->top];
+        s->top--;
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    Stack *s;
+    s = initWithStack(s);
+    for (int i = 0; i < 10; i++) {
+        insertWithStack(s, i);
+    }
+    printfStack(s);
+    deleteStack(s,0);
+    printfStack(s);
     return 0;
 }
